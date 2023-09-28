dbba2207af172c33384765febb6a4dee6cf657b9
dino1729 <dino.feynman@gmail.com>
2023-09-27 20:11:02 -0700

    Fixed GPT4 streaming issue

--- utils/index.ts
+++ utils/index.ts
@@ -39,15 +39,15 @@ export const OpenAIStream = async (prompt: string) => {
       "messages": [
         {
           "role": "system",
-          "content": "You are an intelligent and helpful assistant that accurately answers queries using my memory palace – a location where my personal learnings are stored. You will be provided with a subset of passages from this memory database, which could contain the most likely answer to my query. Please use the context provided to form your answer, but try to avoid copying word-for-word from the passages. Use your own knowledge database only if you don't find a relavant answer in the provided context and keep your answer concise, using no more than 10 sentences."
+          "content": "You are an intelligent and helpful assistant that accurately answers queries using my memory palace – a location where my personal learnings are stored. You will be provided with a subset of passages from this memory database, which could contain the most likely answer to my query. Please use the context provided to form your answer, but try to avoid copying word-for-word from the passages. Use your own knowledge database only if you don't find a relavant answer in the provided context and keep your answer concise."
         },
         {
           "role": "user",
           "content": prompt,
         },
       ],
-      "max_tokens": 840,
-      "temperature": 0.4,
+      "max_tokens": 2048,
+      "temperature": 0.3,
       "stream": true,
     })
   });
@@ -70,78 +70,19 @@ export const OpenAIStream = async (prompt: string) => {
     }
   }
 
-  // const stream = new ReadableStream({
-  //   async start(controller) {
-
-  //     let timeoutId: NodeJS.Timeout | undefined;
-  //     let isControllerClosed = false;
-
-  //     const onParse = (event: ParsedEvent | ReconnectInterval) => {
-  //       if (event.type === "event") {
-  //         const data = event.data;
-
-  //         // if (data === "[DONE]") {
-  //         //   controller.close();
-  //         //   return;
-  //         // }
-
-  //         // try {
-  //         //   const json = JSON.parse(data);
-  //         //   //console.log(json);
-  //         //   const text = json.choices[0].delta.content;
-  //         //   //const text = json.choices[0].text;
-  //         //   //console.log(text);
-  //         //   const queue = encoder.encode(text);
-  //         //   controller.enqueue(queue);
-  //         // } catch (e) {
-  //         //   controller.error(e);
-  //         // }
-  //         try {
-  //           const json = JSON.parse(data);
-  //           if (json.choices[0].finish_reason != null || data === '[DONE]') {
-  //             if (!isControllerClosed) { // Check if controller is not already closed
-  //               clearTimeout(timeoutId);
-  //               controller.close();
-  //               isControllerClosed = true; // Set the flag to true
-  //             }
-  //             return;
-  //           }
-  //           const text = json.choices[0].delta.content;
-  //           const queue = encoder.encode(text);
-  //           controller.enqueue(queue);
-  //         } catch (e) {
-  //           controller.error(e);
-  //         }
-
-  //       }
-  //     };
-
-  //     const parser = createParser(onParse);
-
-  //     for await (const chunk of res.body as any) {
-  //       parser.feed(decoder.decode(chunk));
-  //     }
-
-  //     // Set the timeout to close the controller after a specified duration (e.g., 10 seconds)
-  //     const timeoutDuration = 1000; // 1 seconds (adjust as needed)
-  //     timeoutId = setTimeout(() => {
-  //       // Only close the controller if it's not already closed
-  //       if (!isControllerClosed) {
-  //         controller.close();
-  //         isControllerClosed = true; // Set the flag to true
-  //       }
-  //     }, timeoutDuration);
-
-  //   },
-  // });
-
   const stream = new ReadableStream({
     async start(controller) {
       const onParse = (event: ParsedEvent | ReconnectInterval) => {
         if (event.type === 'event') {
           const data = event.data;
   
-          //console.log(data);
+          // console.log(data);
+          // console.log('--------------------');
+
+          if (data === '[DONE]') {
+            console.log("End of stream");
+            return;
+          }
   
           try {
             const json = JSON.parse(data);
@@ -157,8 +98,11 @@ export const OpenAIStream = async (prompt: string) => {
             }
           } catch (e) {
             // Handle JSON parsing or other errors more gracefully
-            //console.error("Error processing JSON data:", e);
-            controller.error(e);
+            console.error('Error processing JSON data:', e);
+  
+            // Close the controller when a JSON parsing error occurs
+            controller.close();
+            return;
           }
         }
       };
